77372e49587426cc763d2aaaa6d73b608c11230e
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-09-07 17:22:49 +0200

    Add header

--- src/app/controller.js
+++ src/app/controller.js
@@ -1,5 +1,7 @@
 const logger = require('module-tsl-logger');
 
+const header = '<strong>-- Last week interesting data facts --</strong>';
+
 const sequential = funcs =>
   funcs.reduce((promise, func) =>
     promise.then(result =>
@@ -10,7 +12,7 @@ module.exports = (hipchat, queries) => {
   const send2hipchat = (msg) => hipchat.send(msg, "green", true);
   const runStats = (req, res, next) =>
     sequential(queries)
-      .then((report) => send2hipchat(report.join('<br />')))
+      .then((report) => send2hipchat([header].concat(report).join('<br />')))
       .then(msg => res.send(msg));
 
   return {
