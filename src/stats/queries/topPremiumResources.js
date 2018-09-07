dca5dfe89d52320e95a91d9ddf4a4337f68440ec
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-09-07 13:26:57 +0200

    Format topPremiumResource

--- src/stats/queries/topPremiumResources.js
+++ src/stats/queries/topPremiumResources.js
@@ -2,18 +2,18 @@ const extractResources = (rows, field) => rows.map((row) => row[field] || '');
 
 module.exports = ({ mysql }) => () =>
   mysql.query(`
-    SELECT resource_id
-    FROM (
-      SELECT
+    SELECT
          resource_id,
+         resource_name,
          sum(buys) as totals
       FROM resource_by_author_by_day
-      WHERE start_date > '2018-08-01'
-      GROUP BY resource_id ORDER BY totals DESC
-    ) A LIMIT 10
+      WHERE start_date > DATE_ADD(CURDATE(), INTERVAL -7 DAY)
+      GROUP BY resource_id, resource_name ORDER BY totals DESC
+      LIMIT 10;
     `)
     .then((rows) => {
-      console.log(rows)
-      return `Top 10 premium resources of the week ${extractResources(rows, 'resource_id')}`
+      const message = `Top 10 premium resources of the week:`
+      const list = rows.map(row => `&nbsp;&nbsp;&nbsp;&nbsp;${row['totals']} buys - <a href="https://www.tes.com/teaching-resource/resource-${row['resource_id']}" >${row['resource_name']}</a>`)
+      return [message].concat(list).join('<br/>')
     })
     .catch(console.log);
