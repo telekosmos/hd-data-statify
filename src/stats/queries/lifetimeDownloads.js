ad7f7de01271c68f4442c3d1faab8737d1b4c58a
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-09-07 13:07:49 +0200

    format number

--- src/stats/queries/lifetimeDownloads.js
+++ src/stats/queries/lifetimeDownloads.js
@@ -1,6 +1,7 @@
 const extract = (rows, field) => (rows[0] && +rows[0][field]) || 0;
+const format = (str) => parseInt(str).toLocaleString();
 
 module.exports = ({ mysql }) => () =>
   mysql.query('SELECT SUM(lifetime_downloads) AS total_downloads FROM resource_totals')
-    .then((rows) => `${extract(rows, 'total_downloads')} lifetime resources downloads`)
+    .then((rows) => `${format(extract(rows, 'total_downloads'))} lifetime resources downloads`)
     .catch(console.log);
