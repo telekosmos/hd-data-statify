e7c7120bf2160fa4e25b7c25d8e486b457f0d2e9
Josep Servat <jservatlorca@gmail.com>
2018-09-07 12:56:27 +0200

    premium by buys no downloads

--- src/stats/queries/topPremiumResources.js
+++ src/stats/queries/topPremiumResources.js
@@ -1,4 +1,4 @@
-const extractResources = (rows, field) => rows.map((row) => row[field] || 0);
+const extractResources = (rows, field) => rows.map((row) => row[field] || '');
 
 module.exports = ({ mysql }) => () =>
   mysql.query(`
@@ -6,14 +6,13 @@ module.exports = ({ mysql }) => () =>
     FROM (
       SELECT
          resource_id,
-         sum(downloads) as totals
+         sum(buys) as totals
       FROM resource_by_author_by_day
       WHERE start_date > '2018-08-01'
       GROUP BY resource_id ORDER BY totals DESC
     ) A LIMIT 10
     `)
     .then((rows) => {
-
       console.log(rows)
       return `Top 10 premium resources of the week ${extractResources(rows, 'resource_id')}`
     })
